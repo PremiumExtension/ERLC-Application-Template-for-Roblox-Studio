cf1e2e73977ce66f0d01d842133c7aa96bde0de1
PremiumExtension <tuazonangelo64@gmail.com>
2022-11-20 07:17:14 +0800

    Fixed Script Answer Error

--- Source/Server.lua
+++ Source/Server.lua
@@ -1,6 +1,7 @@
 local webhookurl = 'https://discord.com/api/webhooks/1043120444217241640/TELZBbykOh5Ryv55HutciEsyPZga8XjmIFJTRdXHPxikr-YIwjM9IfX57bSnDnCu6HIm'
 local HttpService = game:GetService('HttpService')
 local function request(player,API)
+	
 	local Embedded = {
 		["content"] = "<@0>", -- Replace 0 with the role id of moderators; remove '["content"] = "<@0>",' if you don\'t want to be mentioned.'
 		["embeds"] = {{
@@ -10,10 +11,12 @@ local function request(player,API)
 	}
 	for _,con in pairs(API:split('/')) do
 		if con:find('=') then
-			local QUESTION = con:split('=')[1]
-			local ANSWER = con:split('=')[2]
-			local TIME = ANSWER:split(':')[2]
-			ANSWER = ANSWER:split(':')[1]
+			local QUESTION,ANSWER = con:split('=')[1],nil
+			local TAB2 = con:split('=')[2]
+			local TIME = TAB2:split(':')[2]
+			if TAB2:split(':')[1] then
+				ANSWER = TAB2:split(':')[1]
+			end
 			Embedded.embeds[1].description = Embedded.embeds[1].description..'\n**Q: '..QUESTION..'**\n> **A: **'..ANSWER..'\n> **Speed: **'..TIME..'\n'
 		else
 			continue
